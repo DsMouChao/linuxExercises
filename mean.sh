03c5cc78e4a6380d7108ebb35658fdad38743761
Lars Schimmelpfennig <lschimmelpfe@wisc.edu>
2024-02-29 13:58:24 -0600

    updated mean.sh to also accept stdin inplace of file name

--- mean.sh
+++ mean.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 
-if [[ $# -ne 2 ]]; then # Recall: "-ne" checks integer inequality.
+if [[ $# -lt 1 ]] || [[ $# -gt 2 ]]; then # Recall: "-ne" checks integer inequality.
 	echo "usage: $0 <column> [file.csv]" 1>&2 # write error message to stderr (below)
 	exit 0
 fi
 
 col_idx=$1
-file=$2
+if [[ $# -eq 2 ]]; then
+    # If a file is specified, use it as input
+    input_source="$2"
+else
+    # Otherwise, read from stdin
+    input_source="/dev/stdin"
+fi
+
+#file=$2
 
 #echo $col_idx
 #echo $file
@@ -16,11 +24,27 @@ sum=0
 count=0
 
 # Redirect the output into the while loop
-while IFS= read -r line; do
-    sum=$(($sum + $line))
-    ((count++))
+#while IFS= read -r line; do
+#    sum=$(($sum + $line))
+#    ((count++))
 #    echo $sum
 #    echo $count
-done < <(cut -d "," -f $col_idx $file | tail -n +2)
+#done < <(cut -d "," -f $col_idx $file | tail -n +2)
+
+# Function to process lines and calculate sum and count
+process_lines() {
+    while IFS= read -r line; do
+        if [[ -n $line ]]; then
+            sum=$((sum + line))
+            ((count++))
+       	fi
+
+	done
+}
+
+
+process_lines < <(cut -d "," -f $col_idx $input_source | tail -n +2)
+
+
 
 echo $(($sum/$count))
