f974748095c33baa915efc686f24a4e99bd68ce6
Lars Schimmelpfennig <lschimmelpfe@wisc.edu>
2024-02-29 13:42:26 -0600

    Completed mean.sh and added mtcars.csv

--- mean.sh
+++ mean.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+
+if [[ $# -ne 2 ]]; then # Recall: "-ne" checks integer inequality.
+	echo "usage: $0 <column> [file.csv]" 1>&2 # write error message to stderr (below)
+	exit 0
+fi
+
+col_idx=$1
+file=$2
+
+#echo $col_idx
+#echo $file
+
+sum=0
+count=0
+
+# Redirect the output into the while loop
+while IFS= read -r line; do
+    sum=$(($sum + $line))
+    ((count++))
+#    echo $sum
+#    echo $count
+done < <(cut -d "," -f $col_idx $file | tail -n +2)
+
+echo $(($sum/$count))
